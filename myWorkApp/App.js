887b0fa12ff42d732996470d72c98a040fc88147
PROFF-1 <richmondnyamedor04@gmail.com>
2025-03-14 16:01:15 +0000

    The header section of the screen created and styles

--- myWorkApp/App.js
+++ myWorkApp/App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text,View,Image,TextInput,FlatList} from 'react-native';
+import {styles} from './custom_component/styles';
 
-export default function App() {
+const profileIcon =require('./assets/person.png');
+const filterIcon =require('./assets/filterIcon.png');
+
+//This is the stylesheet of the screen. It has been created as a component
+export default function App(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      //Header of Screen
+       <View style={styles.header}>
+          <View style={styles.headerText}>
+            <Text style={styles.bigHeaderText}>
+              Hello , Devs
+            </Text>
+            <Text style={styles.smallHeaderText}>
+              14 tasks today
+            </Text>
+          </View>
+          <View style ={styles.profileContainer}>
+            <View style={styles.profileIconBackground}>
+              <Image source={profileIcon} style={styles.profileIcon}/>
+            </View>
+          </View>
+       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ myWorkApp/custom_component/styles.js
@@ -0,0 +1,55 @@
+
+//This is the stylesheet of the screen. It has been created as a component
+
+import { StyleSheet,View,Text } from "react-native";
+
+
+export const styles = StyleSheet.create({
+  container:{
+    backgroundColor:'#F7F0E8',
+    flex:1,
+    paddingTop:52,
+  },
+
+  header:{
+    width: 354,
+    height:52,
+    display :'flex',
+    flexDirection:'row',
+    justifyContent:'space-between',
+    marginLeft: 20,
+    marginBottom:30
+  },
+
+  bigHeaderText:{
+    fontSize: 32,
+    fontWeight:700,
+    marginLeft: 20
+  },
+
+  smallHeaderText:{
+    fontSize: 12, 
+    marginLeft: 20
+  },
+
+  profileIconBackground:{
+    backgroundColor:'white',
+    height: 50,
+    width:50,
+    borderRadius: 25,
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'center'
+  },
+
+  profileIconImage:{
+    width:46,
+    height: 45
+  },
+
+  
+})
+
+
+
+
